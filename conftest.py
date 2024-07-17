e3516f0fca24e07ba63473b4548462f25ab777b6
Aslan Pshukov <aslan.pshukov@axxonsoft.dev>
2024-07-17 15:22:11 +0300

    тесты добавления в корзину со страницы товаров с параметризацией

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-import pytest # type: ignore
+import pytest  # type: ignore
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as OptionsFirefox
@@ -10,6 +10,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
 
+
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption('browser_name')
@@ -17,7 +18,8 @@ def browser(request):
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
