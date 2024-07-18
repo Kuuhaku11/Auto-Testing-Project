8959c0908d53cfb04c1dda41ba7a6ac3d77be298
Aslan Pshukov <aslan.pshukov@axxonsoft.dev>
2024-07-18 14:55:28 +0300

    code review

--- test_product_page.py
+++ test_product_page.py
@@ -18,14 +18,15 @@ class TestUserAddToBasketFromProductPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.register_new_user('aasdjhglfhgdfffggfdgfnjha@gmail.py', password)
         login_page.should_be_authorized_user()
-        yield
+        yield  # Отдельно добавил функционал удаления профиля
         self.page.go_to_profile_page()      
         profile_page = ProfilePage(browser, browser.current_url)
         profile_page.delete_profile(password)
         
-
-    def test_user_can_add_product_to_basket(self):
-        page = self.page
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link + '?promo=offer0')
+        page.open()
         page.should_be_add_to_basket_btn()
         page.add_to_basket()
         page.solve_quiz_and_get_code()
@@ -35,33 +36,47 @@ class TestUserAddToBasketFromProductPage():
         page = self.page
         page.should_not_be_success_message()
 
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
+@pytest.mark.need_review
+@pytest.mark.parametrize('num', [0, 1, 2, 3, 4, 5, 6, pytest.param("7", marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, num):
+    page = ProductPage(browser, link + f'?promo=offer{num}')
+    page.open()
+    page.should_be_add_to_basket_btn()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_add_to_basket()
 
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_basket()
-#     page.should_not_be_products()
-#     page.should_be_basket_is_empty_text()
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket()
-#     page.should_not_be_success_message()
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_not_be_products()
+    page.should_be_basket_is_empty_text()
 
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket()
-#     page.should_disappeared()
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
 
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
